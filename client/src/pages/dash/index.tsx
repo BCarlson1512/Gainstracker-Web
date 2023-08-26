b136f429dbcb61ae7145236d37acdef3de9b8bf0
Ben <46061491+BCarlson1512@users.noreply.github.com>
2023-08-25 20:05:33 -0400

    create chart placeholders, style buttons

--- client/src/pages/dash/index.tsx
+++ client/src/pages/dash/index.tsx
@@ -4,7 +4,6 @@ import StatsCard from "~/components/dash/StatsCard";
 import UserCard from "~/components/user/UserCard";
 
 const dash: React.FC = (props) => {
-
     return(
         <>
             <Head>
@@ -24,22 +23,22 @@ const dash: React.FC = (props) => {
                         <h1 className="text-3xl font-extrabold tracking-tight text-white sm:text-[5rem]"> Dashboard </h1>
                         <div className="flex flex-row justify-center items-center">
                             <Link
-                                className="text-xl text-white font-semibold p-4 m-4 border-2 rounded-lg"
-                                href="/create"
-                            >
-                                Create New Workout
-                            </Link>
-                            <Link
-                                className="text-xl text-white font-semibold p-4 m-4 border-2 rounded-lg"
+                                className="text-xl text-white bg-[#33096e] border-[#33096e] font-semibold p-4 m-4 border-2 rounded-lg"
                                 href="/log"
                             >
                                 Log A Workout
                             </Link>
                             <Link
-                                className="text-xl text-white font-semibold p-4 m-4 border-2 rounded-lg"
+                                className="text-xl text-white bg-[#33096e] border-[#33096e] font-semibold p-4 m-4 border-2 rounded-lg"
                                 href="/workouts"
                             >
                                 View Existing Workouts
+                            </Link>
+                            <Link
+                                className="text-xl text-[#33096e] bg-white font-semibold p-4 m-4 border-2 rounded-lg"
+                                href="/create"
+                            >
+                                Create New Workout
                             </Link>
                         </div>
                     </div>
@@ -49,12 +48,28 @@ const dash: React.FC = (props) => {
                         <StatsCard img={"/static/barbell-icon.svg"} statsNum={0} statsMsg={"Sets Completed"}/>
                     </div>
                     <div className="flex flex-row justify-center items-center">
-                        <div className="p-4 mx-2">
-                            <h3 className="text-white text-3xl font-semibold">Weekly Volume</h3>
+                        <div className="border border-gray-100 bg-white rounded-lg drop-shadow-md flex flex-col items-center h-48 p-4 mx-2">
+                            <div className="flex flex-row justify-between">
+                                <h3 className="text-gray-600 text-2xl font-semibold border-b-2 pb-1">Weekly Volume</h3>
+                            </div>
                             {/* TODO: Chartjs charts */}
                         </div>
-                        <div className="p-4 mx-2">
-                            <h3 className="text-white text-3xl font-semibold">Your PR History</h3>
+                        <div className="border border-gray-100 bg-white rounded-lg drop-shadow-md flex flex-col items-center h-48 p-4 mx-2">
+                            <div className="flex flex-row justify-between">
+                                <h3 className="text-gray-600 text-2xl font-semibold border-b-2 pb-1">1 Rep Maxes</h3>
+                            </div>
+                            {/* TODO: Chartjs charts */}
+                        </div>
+                        <div className="border border-gray-100 bg-white rounded-lg drop-shadow-md flex flex-col items-center h-48 p-4 mx-2">
+                            <div className="flex flex-row justify-between">
+                                <h3 className="text-gray-600 text-2xl font-semibold border-b-2 pb-1">Your PR History</h3>
+                            </div>
+                            {/* TODO: Chartjs charts */}
+                        </div>
+                        <div className="border border-gray-100 bg-white rounded-lg drop-shadow-md flex flex-col items-center h-48 p-4 mx-2">
+                            <div className="flex flex-row justify-between">
+                                <h3 className="text-gray-600 text-2xl font-semibold border-b-2 pb-1">Exercises by muscle group</h3>
+                            </div>
                             {/* TODO: Chartjs charts */}
                         </div>
                     </div>
