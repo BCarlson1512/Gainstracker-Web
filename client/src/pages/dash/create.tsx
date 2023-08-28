682f831bd8fa213eeacbd06592a71d0a07a8ccb2
Ben <46061491+BCarlson1512@users.noreply.github.com>
2023-08-27 22:36:18 -0400

    Prelim form styling

--- client/src/pages/dash/create.tsx
+++ client/src/pages/dash/create.tsx
@@ -35,28 +35,31 @@ const Create: React.FC = () => {
                 <title>Create a New Workout</title>
             </Head>
             <DashNav />
-            <main className="flex flex-col ml-24 px-8">
-                <div>
-                    <h1>Create a New Training Plan</h1>
+            <main className="flex flex-col items-center justify-center ml-24 px-8 min-h-screen w-screen bg-gradient-to-b from-[#2e026d] to-[#15162c]">
+                <div className="flex justify-center items-center py-8">
+                    <h1 className="text-white font-bold text-3xl drop-shadow-sm">Create a New Training Plan</h1>
                 </div>
-                <form onSubmit={() => {return;}}>
-                    <div>
-                        <label htmlFor="name">Plan name:</label>
-                        <input id="name" onBlur={(e) => setPlanName(e.target.value)} />
-                    </div>
-                    <div onClick={handleExerciseClick}>Add an Exercise</div>
-                    {planExercises.map((exercise, index) => {
-                        return (
-                            <ExerciseInput 
-                                key={index} 
-                                id={index} 
-                                name={exercise.name} 
-                                muscleGroup={exercise.muscleGrouping}
-                                handleChange={updateExerciseData}
-                            />
-                        )
-                    })}
-                </form>
+                <div>
+                    <form className="flex flex-col justify-center" onSubmit={() => {return;}}>
+                        <div>
+                            <label htmlFor="name" className="text-white px-2 mx-2">Plan name:</label>
+                            <input id="name" className="text-white px-2 mx-2 border rounded-md" onBlur={(e) => setPlanName(e.target.value)} />
+                        </div>
+                        <div className="border rounded-md text-center p-2 m-2 text-white drop-shadow-sm hover:bg-[#33096e] hover:border-[#33096e] transition ease-in" onClick={handleExerciseClick}>Add an Exercise</div>
+                        {planExercises.map((exercise, index) => {
+                            return (
+                                <ExerciseInput 
+                                    key={index} 
+                                    id={index} 
+                                    name={exercise.name} 
+                                    muscleGroup={exercise.muscleGrouping}
+                                    handleChange={updateExerciseData}
+                                />
+                            )
+                        })}
+                        <button type="submit" className="flex justify-center items-center text-white border rounded-md p-2 m-2 text-center hover:bg-[#33096e] hover:border-[#33096e] transition ease-in">Create Training Plan</button>
+                    </form>
+                </div>
             </main>
         </>
     )
