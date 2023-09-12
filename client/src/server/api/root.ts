97ec1a463190c5a447414e4b5b8aff2002434b31
Ben <46061491+BCarlson1512@users.noreply.github.com>
2023-09-12 16:12:22 -0400

    create sets API

--- client/src/server/api/root.ts
+++ client/src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { trainingPlanRouter } from "~/server/api/routers/trainingPlan";
 import { createTRPCRouter } from "~/server/api/trpc";
+import { setRouter } from "./routers/set";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  */
 export const appRouter = createTRPCRouter({
   trainingPlan: trainingPlanRouter,
+  set: setRouter,
 });
 
 // export type definition of API
--- client/src/server/api/schemas/set.ts
+++ client/src/server/api/schemas/set.ts
@@ -3,5 +3,7 @@ import { z } from "zod";
 export const setSchema = z.object({
     weight: z.number().min(0),
     reps: z.number().min(0),
-    unit: z.optional(z.string())
+    unit: z.optional(z.string()),
+    exerciseId: z.string(),
+    userId: z.string(),
 });
