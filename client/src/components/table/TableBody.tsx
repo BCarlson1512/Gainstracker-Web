d8df5d6926a2bbf48726c26bfa111ad0dfbb1204
Ben <46061491+BCarlson1512@users.noreply.github.com>
2023-09-22 17:07:51 -0400

    Tablebody API hookup

--- client/src/components/table/TableBody.tsx
+++ client/src/components/table/TableBody.tsx
@@ -1,7 +1,10 @@
+import Link from "next/link"
+import toast from "react-hot-toast"
 import { AiOutlineDelete } from "react-icons/ai"
 import { CiViewList } from "react-icons/ci"
 import { MdOutlineModeEditOutline } from "react-icons/md"
 import type { Column } from "~/types/Column"
+import { api } from "~/utils/api"
 
 type TableBodyProps = {
     columns: Column[]
@@ -21,7 +24,7 @@ export const TableBody:React.FC<TableBodyProps> = ({columns, tableData}) => {
                                 const tData = data[accessor] ? data[accessor].toString().substring(4, 15): "--"
                                 return <td className="px-6 py-4 font-medium whitespace-nowrap" key={accessor}>{tData}</td>
                             } else if (accessor === "action"){
-                                return <ActionRow key={accessor} />
+                                return <ActionRow key={accessor} id={data.id} />
                             }else {
                                 const tData = data[accessor] ? data[accessor] : "--"
                                 return <td className="px-6 py-4 font-medium whitespace-nowrap" key={accessor}>{tData}</td>
@@ -33,12 +36,38 @@ export const TableBody:React.FC<TableBodyProps> = ({columns, tableData}) => {
     )
 }
 
-const ActionRow = () => {
+type ActionRowProps = {
+    id: string
+}
+
+const ActionRow:React.FC<ActionRowProps> = ({id}) => {
+    
+    const {mutate} = api.workoutLog.deleteLog.useMutation({
+        onSuccess: () => {
+            toast.success("Successfully deleted log")
+        },
+        onError: (err) => {
+            toast.error("Failed to delete log")
+        }
+    })
+
+    const deleteLogHandler = () => {
+        mutate({id: id})
+    }
+
     return (
         <td className="flex items-center justify-center">
-            <AiOutlineDelete />
-            <MdOutlineModeEditOutline />
-            <CiViewList />
+            <AiOutlineDelete size="28" onClick={deleteLogHandler}/>
+            <Link
+                href={`/log/${id}/edit`}
+            >
+                <MdOutlineModeEditOutline size="28"/>
+            </Link>
+            <Link
+                href={`/log/${id}`}
+            >
+                <CiViewList size="28"/>
+            </Link>
         </td>
     )
 }
