496ffed26acc8a3fc0cc2506bfe39f6a680384cf
Ben <46061491+BCarlson1512@users.noreply.github.com>
2023-10-04 23:57:03 -0400

    Update tailwind config

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -4,6 +4,14 @@ export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      'xs': '320px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
   },
   plugins: [],
 } satisfies Config;
